ab5463706631ee66798d2478a8209c116aa1d20f
Standard24 <bitterling@quantentunnel.de>
2017-05-25 19:17:13 +0200

    Allgemeine Formatierungen für Mathematica-Abbildungen geändert, Funktion für ErrorListPlots hinzugefügt

--- evaluation/SoStEval.m
+++ evaluation/SoStEval.m
@@ -1,23 +1,25 @@
 (* ::Package:: *)
 
 BeginPackage["SoStEval`"]
+Needs["ErrorBarPlots`"]
 
-latexStyle::usage=""
+latexStyle::usage="Style[x,FontSize->16,FontFamily->'Latin Modern Roman']"
 plotOptions::usage=""
 expath::usage=""
+makeErrorListPlotData::usage="makeErrorListPlotData[d_,e_]:={#1,ErrorBar[#2]}&@@@Transpose[{d,e}]"
 
 Begin["`Private`"]
 
-latexStyle[x_]:=Style[x,FontSize->16,FontFamily->"Latin Modern Roman"]
+latexStyle[x_]:=Style[x,FontSize->18,FontFamily->"Latin Modern Roman"]
 
-plotOptions={
-Frame->True,
-ImageSize->600,
-FrameTicksStyle->Directive[FontSize->16,FontFamily->"Latin Modern Roman"]
-}
+plotOptions={Frame->True,
+ImageSize->700,
+PlotMarkers->{"\[Bullet]", 15},
+FrameTicksStyle->Directive[FontSize->18,FontFamily->"Latin Modern Roman"]}
 
 expath="../SoStProtokoll/img/"
 
-End[]
+makeErrorListPlotData[d_,e_]:={#1,ErrorBar[#2]}&@@@Transpose[{d,e}]
 
+End[]
 EndPackage[]
